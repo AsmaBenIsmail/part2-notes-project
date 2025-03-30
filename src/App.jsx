d80cdd80cbf85c9d0c396e676742fc4fed0ce1cc
asma <a.s.benismail@lamah.com>
2025-03-30 12:30:35 +0200

    refactor: extract http connection logic to a standalone module

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { useState, useEffect } from 'react'
 import axios from 'axios'
 import Note from './components/Note'
+import noteService from './services/notes'
 
 const App = () => {
   const [notes, setNotes] = useState([])
@@ -8,8 +9,10 @@ const App = () => {
   const [showAll, setShowAll] = useState(true)
 
   useEffect(() => {
-    axios.get('http://localhost:3001/notes').then((response) => {
-      setNotes(response.data)
+    noteService
+    .getAll().
+    then((initialNotes) => {
+      setNotes(initialNotes)
     })
   }, [])
 
@@ -19,9 +22,10 @@ const App = () => {
       content: newNote,
       important: Math.random() > 0.5,
     }
-
-    axios.post('http://localhost:3001/notes', noteObject).then((response) => {
-      setNotes(notes.concat(response.data))
+    noteService
+    .create(noteObject)
+    .then((returnedNote ) => {
+      setNotes(notes.concat(returnedNote))
       setNewNote('')
     })
   }
@@ -31,11 +35,10 @@ const App = () => {
   }
 
   const toggleImportanceOf = (id) => {
-    console.log(`importance of ${id} needs to be toggled`)
-    const url = `http://localhost:3001/notes/${id}`
     const note = notes.find(n=> n.id === id)
     const changedNote = {...note , important: !note.important}
-    axios.put(url, changedNote).then(response => setNotes(notes.map(n => n.id === id ? response.data : n)))
+    noteService.update(id, changedNote)
+   .then(returnedNote  => setNotes(notes.map(n => n.id === id ? returnedNote : n)))
   }
   const notesToShow = showAll ? notes : notes.filter((note) => note.important)
 
